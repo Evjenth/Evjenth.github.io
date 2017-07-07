ac0c486ef231f6192083b2b756166054e3923e44
Andreas Evjenth <anstroen@gmail.com>
2017-07-07 17:11:10 +0200

    s

--- anim/examples/tableView.js
+++ anim/examples/tableView.js
@@ -1,15 +1,34 @@
 var holder = new THREE.Object3D();
 var furObjects = [];
 holder.scale.set(0.01, 0.01, 0.01);
-holder.position.set(0,0,5);
+holder.position.set(0, 0, 5);
 
-load('bottom', [-350, 0, -50]);
-load('side_left', [0, 0, -50]);
-load('side_right', [-350, 0, 0]);
-load('back_plate', [-675, 0, 100]);
-load('top', [0, 0, 300]);
+for (var a = 0; a < 5; a++) {
+    switch (a) {
+        case 0:
+            load('bottom', [-350, 0, -50],a);
+            break;
+        case 1:
+            load('side_left', [0, 0, -50],a);
+            break;
+        case 2:
+            load('side_right', [-350, 0, 0],a);
+            break;
+        case 3:
+            load('back_plate', [-675, 0, 100],a);
+            break;
+        case 4:
+            load('top', [0, 0, 300],a);
+            break;
+    }
+}
 
-function load(name, pos) {
+
+
+
+
+
+function load(name, pos,ind) {
     var path = 'models/obj/';
     var mtlloader = new THREE.MTLLoader();
     var objloader = new THREE.OBJLoader();
@@ -19,24 +38,24 @@ function load(name, pos) {
         materials.preload();
         objloader.load(name + '.obj', function (object) {
             object.position.set(pos[0], pos[1], pos[2]);
-            object.rotation.set(0,0,0);
-            furObjects[furObjects.length] = {
+            object.rotation.set(0, 0, 0);
+            furObjects[ind] = {
                 obj: object,
-                goalPos: [pos[0],pos[1],pos[2]],
-                goalRot: [0,0,0],
-                getRot: function(){
-                    return [object.rotation.x,object.rotation.y,object.rotation.z];
+                goalPos: [pos[0], pos[1], pos[2]],
+                goalRot: [0, 0, 0],
+                getRot: function () {
+                    return [object.rotation.x, object.rotation.y, object.rotation.z];
                 },
-                setRot: function(nRot){
-                    object.rotation.set(nRot[0],nRot[1],nRot[2]);
+                setRot: function (nRot) {
+                    object.rotation.set(nRot[0], nRot[1], nRot[2]);
                 },
                 getPos: function () {
                     return [object.position.x, object.position.y, object.position.z];
                 },
-                setPos : function(nPos){
-                    object.position.set(nPos[0],nPos[1],nPos[2]);
+                setPos: function (nPos) {
+                    object.position.set(nPos[0], nPos[1], nPos[2]);
                 }
-                
+
             };
             holder.add(object);
         });
