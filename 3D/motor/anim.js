e3c6697b028061db5517e70a05d47356f094f8cc
Evjenth <anstroen@gmail.com>
2017-09-04 10:21:14 +0200

    s

--- 3D/motor/anim.js
+++ 3D/motor/anim.js
@@ -1,6 +1,18 @@
 // JavaScript source code
+var scene, renderer, camera;
 
-function init(){
-    alert("HELLO");
+function init() {
+    var container = document.getElementById('container');
+    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 2000);
+    scene = new THREE.Scene();
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+
+    var controls = new THREE.orbitControls(camera, renderer.domElement);
+    controls.target.set(0, 0, 0);
+    camera.position.set(2, 2, 0);
+
+    renderer.render(scene, camera);
 }
 init();
