e751efeebe80d8e7900e5fce9dac995027660905
Evjenth <anstroen@gmail.com>
2017-09-04 10:27:21 +0200

    s

--- 3D/motor/anim.js
+++ 3D/motor/anim.js
@@ -5,6 +5,8 @@ function init() {
     var container = document.getElementById('container');
     camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 2000);
     scene = new THREE.Scene();
+    var light = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
+    scene.add(light);
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     container.appendChild(renderer.domElement);
@@ -12,7 +14,12 @@ function init() {
     var controls = new THREE.orbitControls(camera, renderer.domElement);
     controls.target.set(0, 0, 0);
     camera.position.set(2, 2, 0);
-
     renderer.render(scene, camera);
+    animate();
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    
 }
 init();
