656856fc3ea285070c7fe0bca3cb5a9e0e99e897
Evjenth <anstroen@gmail.com>
2017-09-12 14:17:34 +0200

    Code cleanup
    MainScript has been generalized so it can be re-used, and animation script has been moved to the object (shelf) file and improved.

--- anim/examples/mainScript.js
+++ anim/examples/mainScript.js
@@ -1,44 +1,42 @@
-document.onkeydown = function (key) { reactKey(key); }
-//Keyboard press
-function reactKey(key) {
-    piece(key.keyCode);
-}
-
-var count = 0;
-
-var camera, scene, renderer,
-    object, loader;
+var camera, scene, renderer;
 
 init();
 animate();
 
 function init() {
-    var container = document.getElementById('container');
+    setCamera();
+    setScene();
+    setRenderer();
+    setControls();
+}
 
-    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 100);
+function setControls() {
+    var controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.target.set(0, 3, 0);
+    controls.update();
+}
+
+function setCamera() {
+    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 50);
+    camera.position.set(5, 10, 20);
+    camera.lookAt(new THREE.Vector3(0, 3, 0));
+    window.addEventListener('resize', onWindowResize, false);
+}
+
+function setScene() {
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0xffffff);
-    scene.add(floorMesh);
-    scene.add(hemiLight);
-    scene.add(spotlight);
-    scene.add(spotlight1);
     scene.add(holder);
     scene.add(room);
-    scene.add(pointLight);
+}
+
+function setRenderer() {
     renderer = new THREE.WebGLRenderer({ antialis: true });
     renderer.physicallyCorrectLights = true;
     renderer.gammaInput = true;
     renderer.toneMapping = THREE.ReinhardToneMapping;
     renderer.setSize(window.innerWidth, window.innerHeight);
-    container.appendChild(renderer.domElement);
-
-    var controls = new THREE.OrbitControls(camera, renderer.domElement);
-    controls.target.set(0, 3, 0);
-    camera.position.set(5, 10, 20);
-    controls.update();
-    camera.lookAt(new THREE.Vector3(0, 3, 0));
-    window.addEventListener('resize', onWindowResize, false);
-    renderer.render(scene, camera);
+    document.getElementById('container').appendChild(renderer.domElement);
 }
 
 function onWindowResize() {
@@ -50,38 +48,9 @@ function onWindowResize() {
 function animate() {
     requestAnimationFrame(animate);
     render();
-    renderer.render(scene, camera);
 }
 
 function render() {
-    for (var a = 0; a < furObjects.length; a++) {
-        var pos = furObjects[a].getPos();
-        var goalPos = furObjects[a].goalPos.slice();
-        var rot = furObjects[a].getRot();
-        var goalRot = furObjects[a].goalRot.slice();
-        var equal = true;
-        for (var b = 0; b < pos.length; b++) {
-            if (pos[b] != goalPos[b] || rot[b] != goalRot[b]) {
-                equal = false;
-            }
-        }
-        if (!equal) {
-            var nPos = [];
-            var nRot = [];
-            for (var c = 0; c < 3; c++) {
-                nPos[c] = pos[c] + 0.1 * Math.sign(goalPos[c] - pos[c]);
-                if (Math.abs(goalPos[c] - pos[c]) < 0.1)
-                    nPos[c] = goalPos[c];
-                var diffRot = Math.abs(goalRot[c] - rot[c]);
-                if (diffRot < 0.2) {
-                    nRot[c] = goalRot[c];
-                } else {
-                    nRot[c] = rot[c] + Math.sign((goalRot[c] - rot[c])) / 10;
-                }
-            }
-            furObjects[a].setPos(nPos);
-            furObjects[a].setRot(nRot);
-        }
-    }
-
+    shelfTick();
+    renderer.render(scene, camera);
 }
--- anim/examples/tableView.js
+++ anim/examples/tableView.js
@@ -1,11 +1,11 @@
 var holder = new THREE.Object3D();
 
-var furObjects = [];
+var shelfPiece = [];
 //holder.scale.set(0.01, 0.01, 0.01);
 holder.position.set(0, 0, 5 / 100);
 
 //                               THIS ARRAY IS THE SCALES (X,Y,Z(Y is up)).
-load('bottom', [-3.5, 0, -0.5],0,[0.01,0.01,0.01]);
+load('bottom', [-3.5, 0, -0.5], 0, [0.01, 0.01, 0.01]);
 load('side_left', [0, 0, -0.5], 1, [0.01, 0.01, 0.01]);
 load('side_right', [-3.5, 0, 0], 2, [0.01, 0.01, 0.01]);
 load('back_plate', [-6.75, 0, 1], 3, [0.01, 0.01, 0.01]);
@@ -13,7 +13,7 @@ load('top', [0, 0, 3], 4, [0.01, 0.01, 0.01]);
 holder.matrixAutoUpdate = false;
 holder.updateMatrix();
 
-function load(name, pos, ind,scale) {
+function load(name, pos, ind, scale) {
 
     var path = 'models/obj/';
     var mtlloader = new THREE.MTLLoader();
@@ -24,10 +24,10 @@ function load(name, pos, ind,scale) {
         materials.preload();
         objloader.setMaterials(materials);
         objloader.load(name + '.obj', function (object) {
-            object.scale.set(scale[0],scale[1],scale[2]);
+            object.scale.set(scale[0], scale[1], scale[2]);
             object.position.set(pos[0], pos[1], pos[2]);
             object.rotation.set(0, 0, 0);
-            furObjects[ind] = {
+            shelfPiece[ind] = {
                 obj: object,
                 goalPos: [pos[0], pos[1], pos[2]],
                 goalRot: [0, 0, 0],
@@ -48,4 +48,33 @@ function load(name, pos, ind,scale) {
             holder.add(object);
         });
     });
+}
+
+function shelfTick() {
+    for (var i = 0; i < shelfPiece.length; i++) {
+        var needsUpdate = false;
+        for (var a = 0; a < 3; a++) {
+            if (shelfPiece[i].goalPos[a] != shelfPiece[i].getPos()[a] || shelfPiece[i].goalRot[a] != shelfPiece[i].getRot()[a]) {
+                needsUpdate = true;
+                break;
+            }
+        }
+        if (needsUpdate) {
+            var newPos = shelfPiece[i].getPos().slice();
+            var newRot = shelfPiece[i].getRot().slice();
+            for (var dim = 0; dim < 3; dim++) {
+
+                newPos[dim] += 0.1 * Math.sign(shelfPiece[i].goalPos[dim] - shelfPiece[i].getPos()[dim]);
+                newRot[dim] += (Math.sign(shelfPiece[i].goalRot[dim] - shelfPiece[i].getRot()[dim]) / 10);
+                if (Math.abs(newPos[dim] - shelfPiece[i].goalPos[dim]) < 0.2) {
+                    newPos[dim] = shelfPiece[i].goalPos[dim];
+                }
+                if (Math.abs(newRot[dim] - shelfPiece[i].goalRot[dim]) < 0.2) {
+                    newRot[dim] = shelfPiece[i].goalRot[dim];
+                }
+            }
+            shelfPiece[i].setPos(newPos);
+            shelfPiece[i].setRot(newRot);
+        }
+    }
 }
