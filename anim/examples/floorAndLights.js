9f689782ee595fdc2465e86a7dd3a366aaf19238
Evjenth <anstroen@gmail.com>
2017-09-12 13:20:35 +0200

    s

--- anim/examples/floorAndLights.js
+++ anim/examples/floorAndLights.js
@@ -2,25 +2,39 @@ var ballMat, cubeMat, floorMat, spotlight, spotlight1;
 
 var hemiLight = new THREE.HemisphereLight(0xddeeff, 0x0f0e0d, 10);
 
-var walls = [];
-var wallGeometry = new THREE.PlaneBufferGeometry(20, 10, 20,10);
-var wallMat = new THREE.MeshPhongMaterial({ color: 0xffaaaa });
-for (var i = 0; i < 4 ; i++){
-    walls[i] = new THREE.Mesh(wallGeometry, wallMat);
-   
-}
-walls[0].position.set(0, 5, -10);
-walls[1].rotation.y = Math.PI / 2;
-walls[1].position.set(-10, 5, 0);
-walls[2].rotation.y = - Math.PI / 2;
-walls[2].position.set(10, 5, 0);
-walls[3].rotation.y = Math.PI;
-walls[3].position.set(0, 5, 10);
+var room = buildRoom();
+function buildRoom() {
+    var room = new THREE.Object3D();
+    var walls = [];
+    var wallGeometry = new THREE.PlaneBufferGeometry(20, 10, 20, 10);
+    var wallMat = new THREE.MeshPhongMaterial({ color: "#" + ((1 << 24) * Math.random() | 0).toString(16) });
+    for (var i = 0; i < 5; i++) {
+        walls[i] = new THREE.Mesh(wallGeometry, wallMat);
 
-for (var i = 0; i < 4; i++) {
-    walls[i].matrixAutoUpdate = false;
-    walls[i].updateMatrix();
+    }
+    walls[0].position.set(0, 5, -10);
+    walls[1].rotation.y = Math.PI / 2;
+    walls[1].position.set(-10, 5, 0);
+    walls[2].rotation.y = - Math.PI / 2;
+    walls[2].position.set(10, 5, 0);
+    walls[3].rotation.y = Math.PI;
+    walls[3].position.set(0, 5, 10);
+    walls[4].rotation.x = Math.PI / 2;
+    walls[4].scale.y = 2;
+    walls[4].position.set(0, 10, 0);
+
+    for (var i = 0; i < walls.length; i++) {
+        walls[i].matrixAutoUpdate = false;
+        walls[i].updateMatrix();
+        room.add(walls[i]);
+    }
+
+    return room;
 }
+
+var pointLight = new THREE.PointLight(0xf0f0f0, 10, 100);
+pointLight.position.set(3,3,3);
+
 
 spotlight = new THREE.SpotLight(0xf0f0f0, 200,20,Math.PI/2,0,2);
 spotlight.position.set(0, 8, 0);
