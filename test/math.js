f5b9ebf537d2430cf2527e79c19ab2f7191eae56
Andreas Evjenth <anstroen@gmail.com>
2017-10-17 08:19:37 +0200

    s

--- test/math.js
+++ test/math.js
@@ -1,43 +1,60 @@
-var s = '$$\ sin(u) = {1}/{\ csc(u)}$$' + '<br> ' +
-    '$$\ cos(u) = {1}/{\ sec(u)}$$' + '<br> ' +
-    '$$\ tan(u) = {1}/{\ cot(u)}$$' + '<br> ' +
-    '$$\ csc(u) = {1}/{\ sin(u)}$$' + '<br> ' +
-    '$$\ sec(u) = {1}/{\ cos(u)}$$' + '<br> ' +
-    '$$\ cot(u) = {1}/{\ tan(u)}$$' + '<br> ' +
-    '$$\ sin^2(u) + \ cos^2(u) = 1 $$' + '<br> ' +
-    '$$ 1 + \ tan^2(u) = \ sec^2(u)$$' + '<br> ' +
-    '$$ 1 + \ cot^2(u) = \ csc^2(u)$$' + '<br> ' +
-    '$$ \ tan(u) = {\ sin(u) } / { \ cos(u) } $$' + '<br> ' +
-    '$$ \ cot(u) = {\ cos(u) } / { \ sin(u) } $$' + '<br> ' +
-    '$$ \ sin({{π}/{2}} - u) = \ cos(u) $$' + '<br> ' +
-    '$$ \ cos(π/2 - u) = \ sin(u) $$' + '<br> ' +
-    '$$ \ tan(π/2 - u) = \ cot(u) $$' + '<br> ' +
-    '$$ \ csc(π/2 - u) = \ sec(u) $$' + '<br> ' +
-    '$$ \ sec(π/2 - u) = \ csc(u) $$' + '<br> ' +
-    '$$ \ cot(π/2 - u) = \ tan(u) $$' + '<br> ' +
-    '$$ \ sin(-u) = - \ sin(u) $$' + '<br> ' +
-    '$$ \ cos(-u) = \ cos(u) $$' + '<br> ' +
-    '$$ \ tan(-u) = - \ tan(u) $$' + '<br> ' +
-    '$$ \ csc(-u) = - \ csc(u) $$' + '<br> ' +
-    '$$ \ sec(-u) = \ sec(u) $$' + '<br> ' +
-    '$$ \ cot(-u) = - \ cot(u) $$' + '<br> ' +
-    '$$ \ sin(u ± v) = \ sin(u) \ cos(v) ± \ cos(u) \ sin(v) $$' + '<br> ' +
-    '$$ \ cos(u ± v) = \ cos(u) \ cos(v) ∓ \ sin(u) \ sin(v) $$' + '<br> ' +
-    '$$ \ tan(u ± v) = \ {\ tan(u) ± \ tan(v)}/{ 1 ∓ \ tan(u) \ tan(v)} $$' + '<br> ' +
-    '$$ \ sin(2u) = 2 \ sin(u) \ cos(u) $$' + '<br> ' +
-    '$$ \ cos(2u) = \ cos^2(u) - sin^2(u) $$' + '<br> ' +
-    '$$ \ tan(2u) = {2 \ tan(u) } /{ 1- \ tan^2(u) $$' + '<br> ' +
-    '$$ \ sin^2(u) = {1 - \ cos(2u)} / {2} $$' + '<br> ' +
-    '$$ \ cos^2(u) = {1 + \ cos(2u)} / {2} $$' + '<br> ' +
-    '$$ \ tan^2(u) = {1 - \ cos(2u)} / {1 + cos(2u)} $$' + '<br> ' +
-    '$$ \ sin(u) + \ sin(v) = 2 \ sin({u+v}/2) \ cos({u-v}/2) $$' + '<br> ' +
-    '$$ \ sin(u) - \ sin(v) = 2 \ cos({u+v}/2) \ sin({u-v}/2) $$' + '<br> ' +
-    '$$ \ cos(u) + \ cos(v) = 2 \ cos({u+v}/2) \ cos({u-v}/2) $$' + '<br> ' +
-    '$$ \ cos(u) - \ cos(v) = 2 \ sin({u+v}/2) \ sin({u-v}/2) $$' + '<br> ' +
-    '$$ \ sin(u) \ sin(v) = {1}/2 [\ cos(u -v) - \ cos(u+v)] $$' + '<br> ' +
-    '$$ \ cos(u) \ cos(v) = {1}/2 [\ cos(u-v) + \ cos(u+v)] $$' + '<br> ' +
-    '$$ \ sin(u) \ cos(v) = {1}/2 [\ sin(u+v) + \ sin(u-v)] $$' + '<br> ' +
-    '$$ \ cos(u) \ sin(v) = {1}/2 [\ sin(u+v) - \ sin(u-v)] $$' + '<br> '
+var s = ['$$\ sin(u) = {1}/{\ csc(u)}$$',
+    '$$\ cos(u) = {1}/{\ sec(u)}$$',
+    '$$\ tan(u) = {1}/{\ cot(u)}$$',
+    '$$\ csc(u) = {1}/{\ sin(u)}$$',
+    '$$\ sec(u) = {1}/{\ cos(u)}$$',
+    '$$\ cot(u) = {1}/{\ tan(u)}$$',
+    '$$\ sin^2(u) + \ cos^2(u) = 1 $$',
+    '$$ 1 + \ tan^2(u) = \ sec^2(u)$$',
+    '$$ 1 + \ cot^2(u) = \ csc^2(u)$$',
+    '$$ \ tan(u) = {\ sin(u) } / { \ cos(u) } $$',
+    '$$ \ cot(u) = {\ cos(u) } / { \ sin(u) } $$',
+    '$$ \ sin({{π}/{2}} - u) = \ cos(u) $$',
+    '$$ \ cos(π/2 - u) = \ sin(u) $$',
+    '$$ \ tan(π/2 - u) = \ cot(u) $$',
+    '$$ \ csc(π/2 - u) = \ sec(u) $$',
+    '$$ \ sec(π/2 - u) = \ csc(u) $$',
+    '$$ \ cot(π/2 - u) = \ tan(u) $$',
+    '$$ \ sin(-u) = - \ sin(u) $$',
+    '$$ \ cos(-u) = \ cos(u) $$',
+    '$$ \ tan(-u) = - \ tan(u) $$',
+    '$$ \ csc(-u) = - \ csc(u) $$',
+    '$$ \ sec(-u) = \ sec(u) $$',
+    '$$ \ cot(-u) = - \ cot(u) $$',
+    '$$ \ sin(u ± v) = \ sin(u) \ cos(v) ± \ cos(u) \ sin(v) $$',
+    '$$ \ cos(u ± v) = \ cos(u) \ cos(v) ∓ \ sin(u) \ sin(v) $$',
+    '$$ \ tan(u ± v) = \ {\ tan(u) ± \ tan(v)}/{ 1 ∓ \ tan(u) \ tan(v)} $$',
+    '$$ \ sin(2u) = 2 \ sin(u) \ cos(u) $$',
+    '$$ \ cos(2u) = \ cos^2(u) - sin^2(u) $$',
+    '$$ \ tan(2u) = {2 \ tan(u) } /{ 1- \ tan^2(u) $$',
+    '$$ \ sin^2(u) = {1 - \ cos(2u)} / {2} $$',
+    '$$ \ cos^2(u) = {1 + \ cos(2u)} / {2} $$',
+    '$$ \ tan^2(u) = {1 - \ cos(2u)} / {1 + cos(2u)} $$',
+    '$$ \ sin(u) + \ sin(v) = 2 \ sin({u+v}/2) \ cos({u-v}/2) $$',
+    '$$ \ sin(u) - \ sin(v) = 2 \ cos({u+v}/2) \ sin({u-v}/2) $$',
+    '$$ \ cos(u) + \ cos(v) = 2 \ cos({u+v}/2) \ cos({u-v}/2) $$',
+    '$$ \ cos(u) - \ cos(v) = 2 \ sin({u+v}/2) \ sin({u-v}/2) $$',
+    '$$ \ sin(u) \ sin(v) = {1}/2 [\ cos(u -v) - \ cos(u+v)] $$',
+    '$$ \ cos(u) \ cos(v) = {1}/2 [\ cos(u-v) + \ cos(u+v)] $$',
+    '$$ \ sin(u) \ cos(v) = {1}/2 [\ sin(u+v) + \ sin(u-v)] $$',
+    '$$ \ cos(u) \ sin(v) = {1}/2 [\ sin(u+v) - \ sin(u-v)] $$'];
 
-M.parseMath(s);
-document.write(s);
+write("reciprocal identities:", 0, 6);
+write("pythagorean identities:",6,9);
+write("quotient identites:",9,11);
+write("Co-function identities:",11,17);
+write("Even-odd identities:",17,23);
+write("Sum-difference formulas:",23,26);
+write("Double angle formulas:",26,29);
+write("Power-reducing/half angle formulas",29,32);
+write("Sum-to-product formulas",32,36);
+write("Product-to-sum formulas",36,40);
+
+function write(header, start, end) {
+    document.writeln(header);
+    for (var a = start; a < end; a++) {
+        M.parseMath(s[a]);
+        document.write(s[a]);
+        document.write("<br>");
+    }
+}
